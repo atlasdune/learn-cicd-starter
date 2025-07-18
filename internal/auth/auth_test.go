48521a1b244a058059adc29d8059e547b737e075
atlasdune <raphalbusiness@gmai.com>
2025-07-18 10:30:34 +0100

    broken tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,25 @@
+package auth
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestAuth(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{input: "ApiKey YWxhZGRpbjpvcGVuc2VzYW1l", want: "zYWxhZGRpbjpvcGVuc2VzYW1l"},
+		{input: "", want: "z"},
+	}
+	for _, tc := range tests {
+		header := http.Header{}
+		header.Set("Authorization", tc.input)
+		got, _ := GetAPIKey(header)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Fatalf("Want | %s, got | %s", tc.want, got)
+		}
+	}
+}
